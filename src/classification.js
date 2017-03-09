853934a7ed0d9228fa57003a6362c99743796b35
David Chen <davidchen9568@gmail.com>
2017-03-10 01:07:38 +0800

    Add token grouping policy.

--- src/classification.js
+++ src/classification.js
@@ -1,17 +1,30 @@
 const parser = require('./parser')
 const util = require('./util')
+const grouping = require('./grouping')
 
 function classifiedTokens(text) {
     let tokens = parser.parse(text)
-    let groups = util.groupBy(tokens, t => t.type)
-
-    let stats = new Map()
-    groups.keys().forEach(key => {
-        let val = groups[key]
-        let occurence = val.length
-        let sd = util.stdDeviation(val.map(t => t.lineNum))
-        stats[key] = [occurence, sd]
+    let groups = groupByPolicy(tokens)
+    let stats = groups.map(tokensInType => {
+        let occurence = tokensInType.length
+        let sd = util.stdDeviation(tokensInType.map(t => t.lineNum))
+        return [occurence, sd]
     })
 
     return stats
+}
+
+function groupByPolicy(tokens) {
+    let groups = []
+    tokens.forEach(t => {
+        let mappedType = grouping.fromTokenTypes[t.type]
+        let g = groups[mappedType]
+        if (g === undefined) {
+            g = [t]
+        } else {
+            g.push(t)
+        }
+    })
+    
+    return groups
 }
---
+++ src/grouping.js
@@ -0,0 +1,49 @@
+const parser = require('./parser')
+const util = require('./util')
+const range = util.range
+
+let displayTypes = {
+    propertyMethod: 0,
+    fieldOrLocal: 1,
+    ifSwitchCase: 2,
+    whileForLoop: 3,
+    classStructInterface: 4,
+    emptyLine: 5,
+    comment: 6,
+    stringLiteral: 7,
+    tryCatchFinally: 8,
+    using: 9,
+    assignment: 10,
+    instantiation: 11,
+    return: 12
+}
+
+// For example, this maps '9' to 5 means that 9th element of parser.TokenType is grouped as 
+// 5th element of displayTypes.
+let fromTokenTypes = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 2,
+    '5': 3,
+    '6': 0,
+    '7': 4,
+    '8': 4,
+    '9': 5,
+    '10': 6,
+    '11': 7,
+    '12': 8,
+    '13': 9,
+    '14': 10,
+    '15': 11,
+    '16': 12,
+}
+
+let displayNames = [ 'property/method', 'field/local variable', 'if/switch case', 'while/for loop',
+    'class/struct/interface', 'empty line', 'comment', 'string literal', 'try/catch/finally', 
+    'using', 'assignment', 'instantiation', 'return']
+
+exports.displayTypes = displayTypes
+exports.displayNames = displayNames
+exports.fromTokenTypes = fromTokenTypes
